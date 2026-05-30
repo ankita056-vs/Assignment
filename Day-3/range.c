e7d8d207954b595cbf3e77e3197a088b6306be35
Ankita <ankita91056@gmail.com>
2026-05-30 16:04:08 +0530

    Add files via upload

---
+++ Day-3/range.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+    int start, end, i, j, flag;
+     
+    printf("enter starting number: ");
+    scanf("%d", &start);
+
+    printf("enter ending number: ");
+    scanf("%d", &end);
+
+    for(i = start; i <= end; i++) {
+        if(i <= 1)
+        continue;
+
+        flag = 1;
+
+        for (j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+                break;
+            }
+        }
+        if (flag == 1)
+           printf("%d", i);
+    }
+    return 0;
+}
+
+
